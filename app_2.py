ade940bfa4346dabbc79d34e8f108de5bc486371
Jacks2099 <116288286+Jacks2099@users.noreply.github.com>
2022-10-20 09:02:20 -0600

    Add files via upload

---
+++ app_2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from img_classification import teachable_machine_classification
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+st.title('Aplicación de reconocimiento de Pizza')
+
+uploaded_file = st.file_uploader("Carga una imagen ...", type=["jpg","jpeg"])
+if uploaded_file is not None:
+  image = Image.open(uploaded_file)
+  st.sidebar.image(image)
+  label = teachable_machine_classification(image, 'keras_model.h5') 
+  if label == 0;
+    st.write('Es una pizza')
+  
+  else:
+    st.write('No es pizza')
